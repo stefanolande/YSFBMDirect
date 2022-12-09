e6806fbc1372df7262de33b804f9347a6275851f
Stefano Lande <landeste@gmail.com>
2022-12-09 10:32:04 +0100

    Add sanity check to DG-ID configuration

--- pYSFBMGateway.py
+++ pYSFBMGateway.py
@@ -6,7 +6,7 @@ import sys
 import threading
 import time
 
-from utils import now, pad
+from utils import now, pad, validate_dg_id_map
 from ysf import ysffich
 from ysfd_protocol import send_group_message, login_and_set_tg
 
@@ -54,9 +54,9 @@ def ysf_to_bm():
             ysffich.decode(data[40:])
             dg_id = ysffich.getSQ()
 
-            if cur_dg_id != dg_id and dg_id in dgid_tg:
-                logging.info(f"Changing TG to {dgid_tg[dg_id]} mapped from DG-ID {dg_id}")
-                send_group_message(callsign, dgid_tg[dg_id], bm_sock)
+            if cur_dg_id != dg_id and dg_id in dgid_to_tg:
+                logging.info(f"Changing TG to {dgid_to_tg[dg_id]} mapped from DG-ID {dg_id}")
+                send_group_message(callsign, dgid_to_tg[dg_id], bm_sock)
                 set_dg_id(dg_id)
 
         bm_sock.send(data)
@@ -75,7 +75,14 @@ def send_ping(call: str):
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
-    config.read("ysfdirect.conf")
+    config.read("pYSFBMGateway.conf")
+
+    loglevel = config["LOG"]["loglevel"]
+    if config["LOG"]["logtype"] == "stdout":
+        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=loglevel)
+    else:
+        file = config["LOG"]["path"]
+        logging.basicConfig(filename=file, format='%(asctime)s - %(levelname)s - %(message)s', level=loglevel)
 
     callsign = config["CONNECTION"]["callsign"]
     server_ip = config["CONNECTION"]["server_ip"]
@@ -86,26 +93,25 @@ if __name__ == '__main__':
 
     default_tg = int(config["TG"]["default_tg"])
 
-    dgid_tg = {int(k): int(v) for k, v in config["DGID-TO-TG"].items()}
+    dgid_to_tg = {int(k): int(v) for k, v in config["DGID-TO-TG"].items()}
 
-    tg_dgid = {v: k for k, v in dgid_tg.items()}
+    if not validate_dg_id_map(dgid_to_tg):
+        message = "DGID-TO-TG configuration is invalid - check for duplicated TGs"
+        logging.error(message)
+        print(message)
+        sys.exit(1)
+
+    tg_to_dgid = {v: k for k, v in dgid_to_tg.items()}
 
     client_addr = ""
     last_client_packet_timestamp = 0
-    cur_dg_id = tg_dgid[default_tg]
+    cur_dg_id = tg_to_dgid[default_tg]
 
     bm_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     bm_sock.connect((server_ip, bm_port))
 
     ysf_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ysf_sock.bind(("", ysf_port))
-
-    loglevel = config["LOG"]["loglevel"]
-    if config["LOG"]["logtype"] == "stdout":
-        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=loglevel)
-    else:
-        file = config["LOG"]["path"]
-        logging.basicConfig(filename=file, format='%(asctime)s - %(levelname)s - %(message)s', level=loglevel)
 
     logging.info("Starting pYSFBMGateway")
     logging.info(f"Default TG {default_tg} mapped to DG-ID {cur_dg_id}")
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def now():
     dt = datetime.now()
     return datetime.timestamp(dt)
@@ -8,3 +9,8 @@ def now():
 def pad(data: bytes, length: int) -> bytes:
     padding_length = length - len(data)
     return data + b'\x20' * padding_length
+
+
+def validate_dg_id_map(dgid_to_tg_map: dict) -> bool:
+    # checks that a TG appears only one
+    return len(set(dgid_to_tg_map.values())) == len(dgid_to_tg_map)
