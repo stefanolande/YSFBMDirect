70b81937d85b07be374c3787a4aab0f87d430ea7
Stefano Lande <stefano.lande@commercetools.com>
2022-12-16 18:47:44 +0100

    fix Unlink message

--- ysfd_protocol.py
+++ ysfd_protocol.py
@@ -34,7 +34,7 @@ def login_and_set_tg(callsign, bm_password, tg, bm_sock, is_salt_received, maybe
     send_tg_message(callsign, tg, bm_sock)
 
 def send_logout_message(call: str, bm_sock):
-    message = "YSFLU".encode() + pad(call.encode(), 10)
+    message = "YSFU".encode() + pad(call.encode(), 10)
 
     for i in range(1,3):
         logging.debug("sending: %s" % message)
