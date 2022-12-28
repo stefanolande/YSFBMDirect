cb465c6fe4628fd6aae1a068068ec2ebc9d8b1a0
Stefano Lande <stefano.lande@commercetools.com>
2022-12-28 20:17:26 +0100

    minor install script

--- install.sh
+++ install.sh
@@ -4,29 +4,29 @@
 
 rm -rf /tmp/YSFBMDirect
 
-cd /tmp/
+cd /tmp/ || exit
 git clone https://github.com/stefanolande/YSFBMDirect.git
 cp YSFBMDirect/YSFBMDirect.conf.example YSFBMDirect/YSFBMDirect.conf
 
 
 while [[ $call == '' ]]
 do
-    read -p "Enter your callsign: " call
+    read -r -p "Enter your callsign: " call
 done
 
 while [[ $password == '' ]]
 do
-	read -p "Enter your Brandmeister password: " password
+	read -r -p "Enter your Brandmeister password: " password
 done
 
 sed -i "s/CALLSIGN/$call/" YSFBMDirect/YSFBMDirect.conf
 sed -i "s/PASSWORD/$password/" YSFBMDirect/YSFBMDirect.conf
 
-read -p "Do you want to edit the Talk Group configuration [y/N]?" edit_tg
+read -r -p "Do you want to edit the Talk Group configuration [y/N]?" edit_tg
 
-while [[ $edit_tg != 'y' ]] && [[ $edit_tg != 'Y' ]] && [[ $edit_tg != 'n' ]] && [[ $edit_tg != 'N' ]] && [[ $string != '' ]]
+while [[ $edit_tg != 'y' ]] && [[ $edit_tg != 'Y' ]] && [[ $edit_tg != 'n' ]] && [[ $edit_tg != 'N' ]] && [[ $edit_tg != '' ]]
 do
-	read -p "Do you want to edit the Talk Group configuration [y/N]?" edit_tg
+	read -r -p "Do you want to edit the Talk Group configuration [y/N]?" edit_tg
 done
 
 if [[ $edit_tg == 'y' ]] || [[ $edit_tg == 'Y' ]];
@@ -36,8 +36,9 @@ else
 	echo "You can edit it later in /opt/YSFBMDirect/YSFBMDirect.conf"
 fi
 
-mv -f YSFBMDirect /opt/
-cd /opt/YSFBMDirect/
+rm -rf /opt/YSFBMDirect
+cp -r YSFBMDirect /opt/
+cd /opt/YSFBMDirect/ || exit
 cp ysfbmdirect.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable ysfbmdirect.service
